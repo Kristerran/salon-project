83d26e0d0ce1d0eaf8a22b89164b3ecb6e69782c
Nikki Mounce <nikkimounce@gmail.com>
2021-11-24 14:15:41 -0800

    added react script for social icons

---
+++ src/components/social-icons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import '../css/social-icons';
+
+import { AiFillFacebook, AiFillTwitterSquare, AiFillInstagram } from "react-icons/ai";
+import { FaYelp } from "react-icons/fa";
+
+function SocialIcons() {
+
+    return (
+        <Container>
+            <ul>
+                <li><a href="https://facebook.com"><AiFillFacebook /></a></li>
+                <li><a href="https://twitter.com"><AiFillTwitterSquare /></a></li>
+                <li><a href="https://instagram.com"><AiFillInstagram /></a></li>
+                <li><a href="https://yelp.com"><FaYelp /></a></li>
+            </ul>
+        </Container>
+    );
+}
+export default SocialIcons;
+
+//dont add any background styling to the social icon container
