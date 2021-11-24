9c2189560ffd4f306b9936f70b647de08e056a43
Nikki Mounce <nikkimounce@gmail.com>
2021-11-24 13:11:56 -0800

    added navbar react script

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../css/nav.css';
 
-import { FaHome, Fa } from 'react-icons/fa';
+//feel free to change the icons, just be sure to update the icons call tags in
+//the script below
+import { FaHome} from 'react-icons/fa';
 import { MdMenuBook, MdOutlineLocalGroceryStore } from 'react-icons/md';
 import { CgProfile } from 'react-icons/cg';
 import { IoStorefrontSharp, IoLoginOutline } from 'react-icons/io';
 
-//makes text appear when hovering over nav icons
-function useHover() {
-    const [hovering, setHovering] = useState(false)
-    const onHoverProps = {
-        onMouseEnter: () => setHovering(true),
-        onMouseLeave: () => setHovering(false),
-    }
-    return [hovering, onHoverProps]
-    };
-
 
 function Navbar() {
 
-    const [linkAIsHovering, linkAHoverProps] = useHover()
-    const [linkBIsHovering, linkBHoverProps] = useHover()
-    const [linkCIsHovering, linkCHoverProps] = useHover()
-    const [linkDIsHovering, linkDHoverProps] = useHover()
-    
     return (
-        <Navbar>
-            <Nav className="ml-auto" defaultActiveKey="#home">
-                <Navbar.Brand href="/">INSERT NAME FOR LOGO HERE</Navbar.Brand>
-                <Nav.Item>
-                    <Nav.Link 
-                </Nav.Item>
+
+        <Navbar collapseOnSelect sticky="top" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">INSERT TEXT FOR LOGO HERE</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/home">Home<FaHome /></Nav.Link>
+                        <Nav.Link href="/service-menu"><MdMenuBook />Services</Nav.Link>
+                        <Nav.Link href="/profiles"><CgProfile />Stylists</Nav.Link>
+                        <Nav.Link href="/store"><IoStorefrontSharp />Store</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/login"><IoLoginOutline />Login</Nav.Link>
+                        <Nav.Link href="/cart"><MdOutlineLocalGroceryStore />Cart</Nav.Link>
+                    </Nav>
+            </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
