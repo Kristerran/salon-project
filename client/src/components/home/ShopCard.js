8caad8483ee2785837ffdf981fb325e5cf3c3b07
Nikki Mounce <nikkimounce@gmail.com>
2021-12-01 20:56:56 -0800

    added shop cards section to homepage

---
+++ client/src/components/home/ShopCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, CardText, Row, Col } from 'reactstrap';
+import Placeholder from '../../assets/images/placeholder.png';
+
+function ShopCard() {
+    return (
+        <Row>
+            <Col md={4}>
+                <Card>
+                    <CardImg src={Placeholder} alt="Card image" />
+                    <CardImgOverlay>
+                        <CardTitle>Shampoo & Conditioner</CardTitle>
+                        <CardText> Lorem</CardText>
+                    </CardImgOverlay>
+                </Card>
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <CardImg src={Placeholder} alt="Card image" />
+                    <CardImgOverlay>
+                        <CardTitle>Styling Products</CardTitle>
+                        <CardText>Lorem</CardText>
+                    </CardImgOverlay>
+                </Card>
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <CardImg src={Placeholder} alt="Card image" />
+                    <CardImgOverlay>
+                        <CardTitle>Mens Haircare</CardTitle>
+                        <CardText>Lorem</CardText>
+                    </CardImgOverlay>
+                </Card>
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <CardImg src={Placeholder} alt="Card image" />
+                    <CardImgOverlay>
+                        <CardTitle>Lorem</CardTitle>
+                        <CardText>Hand-Tied</CardText>
+                    </CardImgOverlay>
+                </Card>
+            </Col>
+        </Row>
+    )
+}
+
+export default ShopCard;
