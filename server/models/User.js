2d6643b3270ea8d203938bdfa1f97d9b6078aa18
Laxmi Sharma <laxminla@yahoo.com>
2021-12-01 21:55:52 -0800

    added auth.js file and set up password middleware in user model

--- server/models/User.js
+++ server/models/User.js
@@ -20,15 +20,32 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
     },
     password: {
         type: String,
         required: true,
-        trim: 6,
+        trim: 5,
     },
    orders: [Order.schema], 
 
-})
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+  // compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
