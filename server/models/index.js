34198dbe854842522684809d24f34bb55d96819e
Laxmi Sharma <laxminla@yahoo.com>
2021-12-02 19:10:04 -0800

    modified server and index

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const User = require('./User');
 const Stylist = require('./Stylist');
 const Service = require('./Service');
-const Order = require('./Order')
-const Product = require('./Product')
-const Testimonial = require('./Testimonial')
+const Order = require('./Order');
+const Product = require('./Product');
+const Testimonial = require('./Testimonial');
 module.exports = { User, Stylist, Service, Order, Product, Testimonial };
 
 
