34912cf6e982542e055c003edd3e56326266b1c8
Nikki Mounce <nikkimounce@gmail.com>
2021-12-07 19:37:27 -0800

    updated app.js to include store components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import { Provider } from 'react-redux';
-import store from './storeConfig.js';
+import store from './utils/store';
 
 import './css/App.css';
 import "./css/index.css";
@@ -17,21 +18,34 @@ import Services from './components/services/ServicesContainer.js';
 import items from "./components/services/data.js";
 import About from './components/about/AboutContainer.js';
 import Reviews from './components/reviews/ReviewContainer.js';
-import Store from './components/store/Containers/HomepageContainer';
+import Store from './components/store/pages/Home';
+import Detail from './components/store/pages/Detail';
+import NoMatch from '.components/store/pages/NoMatch';
+import Login from '.components/store/pages/Login';
+import Signup from '.components/store/pages/Signup';
+import Success from '.components/store/pages/Success';
+import OrderHistory from '.components/store/pages/OrderHistory';
 import Footer from './components/main/footer.js';
 
 
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token')
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
+const httpLink = createHttpLink({
   uri: '/graphql',
-})
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
@@ -47,7 +61,13 @@ function App() {
               <Route path='/services' element={<Services items={items} />} />
               <Route path='/about' element={<About />} />
               <Route path='/reviews' element={<Reviews />} />
-              {/* <Route path='/store' element={<Store />} /> */}
+              <Route path='/store' element={<Store />} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
             </Routes>
             <Footer />
           </Provider>
