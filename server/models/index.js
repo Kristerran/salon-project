b4ac37013fd7b0af2d0205f601da3140907f993c
Nikki Mounce <nikkimounce@gmail.com>
2021-12-07 19:41:42 -0800

    added category model

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,8 @@ const Stylist = require('./Stylist');
 const Service = require('./Service');
 const Order = require('./Order');
 const Product = require('./Product');
+const Category = require('./Category');
 const Testimonial = require('./Testimonial');
-module.exports = { User, Stylist, Service, Order, Product, Testimonial };
+module.exports = { User, Stylist, Service, Order, Category, Product, Testimonial };
 
 
